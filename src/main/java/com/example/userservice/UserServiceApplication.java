164321346e479ec9094c8d1f0fd216a2908d5487
이현국 <hklee@sptek.co.kr>
2021-04-28 17:34:12 +0900

    kafka setting

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,14 +1,25 @@
 package com.example.userservice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import com.example.userservice.error.FeignErrorDecoder;
+
+import feign.Logger;
+
+
+
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients	//restTempate를 feignclients로 대체
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
@@ -22,8 +33,19 @@ public class UserServiceApplication {
 	}
 	
 	@Bean
+	@LoadBalanced // 다른 ms와 연동시 서비스id로 호출 가능 ex  : http://127.0.0.1:8000/order-service -> http://order-service/order-service 
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
+	
+	@Bean
+	public Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
+	
+//	@Bean
+//	public FeignErrorDecoder getFeignErrorDecoder() {
+//		return new FeignErrorDecoder();
+//	}
 
 }
--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,27 +7,44 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
 
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService{
 	
 	UserRepository userRepository;
 	BCryptPasswordEncoder pwdEncoder;
+	Environment env;
+	RestTemplate restTemplate;
+	
+	OrderServiceClient orderServiceClient;
 	
 	@Autowired
-	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder pwdEncoder) {
+	public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder pwdEncoder, Environment env, RestTemplate restTemplate, OrderServiceClient orderServiceClient) {
 		this.userRepository = userRepository;
 		this.pwdEncoder = pwdEncoder; 
+		this.env = env;
+		this.restTemplate = restTemplate;
+		this.orderServiceClient = orderServiceClient;
 	}
 	
 	@Override
@@ -55,10 +72,18 @@ public class UserServiceImpl implements UserService{
 		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
 		
 		List<ResponseOrder> orders = new ArrayList<ResponseOrder>();
+		// rest template order 정보 요청 start
+//		List<ResponseOrder> orders = null;
+//		String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+//		
+//		ResponseEntity<List<ResponseOrder>> orderRes = restTemplate.exchange(orderUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<ResponseOrder>>() {
+//		});
+//		orders = orderRes.getBody();
+		// rest template order 정보 요청 end
 		
-		//order 정보 요청 start
+		/*feign client*/ //예외처리는 FeignErrorDecoder에서 처리
+		orders = orderServiceClient.getOrders(userId);
 		
-		//order 정보 요청 end
 		userDto.setOrders(orders);
 		
 		return userDto;
